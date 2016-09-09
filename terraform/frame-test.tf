033c07ecd2e21eef31a4a59a796efeddd3c48439
Justin Headley <justin@scal.io>
2016-09-09 11:49:55 -0700

    terraform update

--- terraform/frame-test.tf
+++ terraform/frame-test.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "tempo-api-dev" {
+resource "aws_instance" "frame-test" {
     ami = "${lookup(var.amis, var.region)}"
-    instance_type = "m4.large"
-    key_name = "tempo-api-dev"
+    instance_type = "t2.micro"
+    key_name = "frame-test"
     tags {
-      Name = "tempo-api-dev"
+      Name = "frame-test"
     }
     security_groups = [
       "${aws_security_group.allow_all.name}"
