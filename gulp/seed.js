1b5207f77245fcdf4870d0515508fb7840840b4a
Justin Headley <justin@scal.io>
2016-10-30 11:38:52 -0700

    Updated seed task to clear users and roles first.

--- gulp/seed.js
+++ gulp/seed.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var exit = require('gulp-exit');
+var Q = require('q');
 var mongoose = require('mongoose');
 var passwordUtility = require('../api/utilities/password-helper');
 
@@ -12,45 +13,64 @@ gulp.task('seed', ['models'], function() {
     var hashedPassword = passwordUtility.hash_password('devdev');
 
     return generateModels(mongoose).then(function(models) {
-        var roles = [
-            {
-                name: "Account",
-                description: "A standard user account."
-            },
-            {
-                name: "Admin",
-                description: "A user with advanced permissions."
-            },
-            {
-                name: "SuperAdmin",
-                description: "A user with full permissions."
-            }
-        ];
-        return models.role.create(roles, function(error, roles) {
-            var users = [
+
+        return dropCollections(models).then(function() {
+            console.log("seeding roles");
+            var roles = [
                 {
-                    email: 'test@account.com',
-                    password: hashedPassword,
-                    role: roles[0]._id
+                    name: "Account",
+                    description: "A standard user account."
                 },
                 {
-                    email: 'test@admin.com',
-                    password: hashedPassword,
-                    role: roles[1]._id
+                    name: "Admin",
+                    description: "A user with advanced permissions."
                 },
                 {
-                    email: 'test@superadmin.com',
-                    password: hashedPassword,
-                    role: roles[2]._id
+                    name: "SuperAdmin",
+                    description: "A user with full permissions."
                 }
             ];
-            return models.user.create(users, function(error, users) {
-                return gulp.src("")
-                .pipe(exit());
+            return models.role.create(roles, function (error, roles) {
+                console.log("seeding users");
+                var users = [
+                    {
+                        email: 'test@account.com',
+                        password: hashedPassword,
+                        role: roles[0]._id
+                    },
+                    {
+                        email: 'test@admin.com',
+                        password: hashedPassword,
+                        role: roles[1]._id
+                    },
+                    {
+                        email: 'test@superadmin.com',
+                        password: hashedPassword,
+                        role: roles[2]._id
+                    }
+                ];
+                return models.user.create(users, function (error, users) {
+                    return gulp.src("")
+                    .pipe(exit());
+                })
             })
+
         })
     })
+
 });
+
+function dropCollections(models) {
+    var deferred = Q.defer();
+    models.user.remove({}, function(err) {
+        console.log('roles removed');
+        models.role.remove({}, function(err) {
+            console.log('users removed');
+            deferred.resolve();
+        });
+    });
+    return deferred.promise;
+}
 
 gulp.task('models', function() {
     return gulp.src('./seed/**/*.*')
