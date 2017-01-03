7655223bbadd711835c68397c8ef86d5f566056d
Justin Headley <justin@scal.io>
2017-01-03 14:12:50 -0800

    no message

--- rest-hapi-seed.js
+++ rest-hapi-seed.js
@@ -4,6 +4,11 @@ var exec = require('child_process').exec;
 
 console.log(__dirname);
 
+exec('echo $PWD', function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+});
+
 // exec('gulp seed', function(err, stdout, stderr) {
 //     console.log(stdout);
 //     console.log(stderr);
