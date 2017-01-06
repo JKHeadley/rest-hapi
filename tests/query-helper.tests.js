9c952701fe0268227e99f7f217c5520bfae3b311
Justin Headley <justin@scal.io>
2017-01-06 01:13:32 -0800

    no message

--- tests/query-helper.tests.js
+++ tests/query-helper.tests.js
@@ -10,6 +10,7 @@ Log.logLevel = "ERROR";
 Log = Log.bind("query-helper");
 var testHelper = require("./test-helper");
 
+//TODO: add tests for text search functions
 
 test('query-helper exists and has expected members', function (t) {
   //<editor-fold desc="Arrange">
