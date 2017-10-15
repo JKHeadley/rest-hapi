4f051991b0f9adc0c578575a803fe6c951767c9e
Justin Headley <justin@scal.io>
2017-10-14 17:59:35 -0700

    testing travisci

--- gulp/test.js
+++ gulp/test.js
@@ -2,18 +2,24 @@ var gulp = require('gulp');
 var tape = require('gulp-tape');
 var tapColorize = require('tap-colorize');
 
-gulp.task('test', ['test-e2e'], function() {
-
-});
-
-gulp.task('test-e2e', ['test-unit'], function() {
+gulp.task('test', [], function() {
   return gulp.src([
-    gulp.paths.src + '/**/end-to-end.tests.js'
+    gulp.paths.src + '/**/*.tests.js',
+    '!' + gulp.paths.src + '/**/end-to-end.tests.js'
   ])
       .pipe(tape({
         reporter: tapColorize()
       }));
 });
+
+// gulp.task('test-e2e', ['test-unit'], function() {
+//   return gulp.src([
+//     gulp.paths.src + '/**/end-to-end.tests.js'
+//   ])
+//       .pipe(tape({
+//         reporter: tapColorize()
+//       }));
+// });
 
 gulp.task('test-unit', function() {
   return gulp.src([
