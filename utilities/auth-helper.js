f21f0dfab11bb3c7275ee052eab8e9ea8816ee8b
Justin Headley <justin@scal.io>
2017-10-25 10:06:58 -0700

    Adding missing permissions.

--- utilities/auth-helper.js
+++ utilities/auth-helper.js
@@ -79,20 +79,20 @@ module.exports = {
 
     const scope = {};
 
-    scope.rootScope = ["root", model.collectionName];
-    scope.createScope = ["create", "create" + modelName];
-    scope.readScope = ["read", "read" + modelName];
-    scope.updateScope = ["update", "update" + modelName];
-    scope.deleteScope = ["delete", "delete" + modelName];
-    scope.associateScope = ["associate", "associate" + modelName];
+    scope.rootScope = ["root", model.collectionName, "!-root", "!-" + model.collectionName];
+    scope.createScope = ["create", "create" + modelName, "!-create", "!-create" + modelName];
+    scope.readScope = ["read", "read" + modelName, "!-read", "!-read" + modelName];
+    scope.updateScope = ["update", "update" + modelName, "!-update", "!-update" + modelName];
+    scope.deleteScope = ["delete", "delete" + modelName, "!-delete", "!-delete" + modelName];
+    scope.associateScope = ["associate", "associate" + modelName, "!-associate", "!-associate" + modelName];
 
     const associations = model.routeOptions.associations;
 
     for (const key in associations) {
       const associationName = key[0].toUpperCase() + key.slice(1);
-      scope["add" + modelName + associationName + "Scope"] = "add" + modelName + associationName;
-      scope["remove" + modelName + associationName + "Scope"] = "remove" + modelName + associationName;
-      scope["get" + modelName + associationName + "Scope"] = "get" + modelName + associationName;
+      scope["add" + modelName + associationName + "Scope"] = ["add" + modelName + associationName, "!-add" + modelName + associationName];
+      scope["remove" + modelName + associationName + "Scope"] = ["remove" + modelName + associationName, "!-remove" + modelName + associationName];
+      scope["get" + modelName + associationName + "Scope"] = ["get" + modelName + associationName, "!-get" + modelName + associationName];
     }
 
     //EXPL: merge any existing scope fields with the generated scope
