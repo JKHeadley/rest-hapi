b14a457abdb3ec75fd3edb46fae14ddfd3bd9a08
Justin Headley <headley.justin@gmail.com>
2018-06-17 14:08:13 -0700

    - Added google analytics tracking id

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -100,6 +100,8 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
+
+  gaTrackingId: 'UA-120977991-1'
 }
 
 module.exports = siteConfig
