a2fe360b31fca05a9309c6031f45fbb34e3a49c1
Justin Headley <headley.justin@gmail.com>
2018-06-17 03:36:20 -0700

    - Fixing image route

--- website/data/users.js
+++ website/data/users.js
@@ -2,7 +2,7 @@ module.exports = [
   // Please add your logo in alphabetical order of caption.
   {
     caption: 'appy',
-    image: '/rest-hapi/img/appy.png',
+    image: '/img/appy.png',
     infoLink: 'https://www.appyapp.io',
     pinned: true
   }
